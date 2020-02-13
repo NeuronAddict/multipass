bd793952c416e333dda09d5e11b58eb0c35bd2a1
neuronaddict <dev@lacourarie.org>
2020-02-13 23:11:42 +0100

    Re add admin

--- multipass/settings.py
+++ multipass/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'exfiltrate.apps.ExfiltrateConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- multipass/urls.py
+++ multipass/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('exfiltrate/', include('exfiltrate.urls')),
+    path('admin/', admin.site.urls),
 ]
