0b873818ccae4d1d4ad4544b4cdc5835d7aa4fe8
neuronaddict <dev@lacourarie.org>
2020-02-13 23:04:55 +0100

    initial model.

--- exfiltrate/models.py
+++ exfiltrate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Credential(models.Model):
+    username = models.CharField(max_length=400)
+    password = models.CharField(max_length=400)
+    found_data = models.DateTimeField(auto_now=True)
+
--- multipass/settings.py
+++ multipass/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'exfiltrate.apps.ExfiltrateConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
