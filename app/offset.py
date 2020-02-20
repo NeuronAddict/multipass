809b9af850aade7b1277acd03d0b459ba99cb15a
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-02-20 18:41:08 +0100

    Fist offset test.

--- app/offset.py
+++ app/offset.py
@@ -6,9 +6,13 @@ from app.models import Offset
 class OffsetUtils:
 
     @classmethod
-    def next_username_offset(cls, now: datetime) -> int:
+    def next_username_offset(cls, client, now: datetime) -> Offset:
         last = Offset.objects.filter(last_send__gt=(now - timedelta(minutes=15)), ack=False).order_by('id')[:1]
 
         if len(last) == 0:
-            return 0
-        return last[0].value
+            offset = Offset(value=0, client=client, type=0)
+        else:
+            offset = Offset(value=last.value+256, client=client, type=0)
+
+        offset.save()
+        return offset
--- app/test_offset.py
+++ app/test_offset.py
@@ -10,11 +10,28 @@ class OffsetTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        domain = Domain(name='victim')
-        domain.save()
-        client = Client(ip='129.168.0.1', user_agent='test', domain=domain)
-        client.save()
+        cls.domain = Domain(name='victim')
+        cls.domain.save()
+        cls.first_client = Client(ip='129.168.0.1', user_agent='tester', domain=cls.domain)
+        cls.first_client.save()
 
-    def test_offset(self):
-        last = OffsetUtils.next_username_offset(datetime(2020, 2, 20))
-        self.assertEqual(last, 0)
+        cls.bogged_client = Client(ip='129.168.0.2', user_agent='bogged tester', domain=cls.domain)
+        cls.bogged_client.save()
+
+    def test_bogged_offset(self):
+        first_offset = OffsetUtils.next_username_offset(self.first_client, datetime(2020, 2, 20, 1, 15, 1))
+        self.assertEqual(first_offset.value, 0)
+
+        first_offset.ack = True
+        first_offset.save()
+
+        second_offset = Offset(value=256, type=0, client=self.bogged_client, last_send=datetime(2020, 2, 20, 0, 0, 0))
+        second_offset.save()
+
+        third_offset = Offset(value=256*2, type=0, client=self.first_client, ack=True)
+        third_offset.save()
+
+        last = OffsetUtils.next_username_offset(self.first_client, datetime(2020, 2, 20, 1, 15, 1))
+        self.assertEqual(last.value, 256)
+
+
