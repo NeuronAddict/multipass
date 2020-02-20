8dcb009f1958d9258ac7be9970e7d5c19446428b
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-02-20 18:36:23 +0100

    Add ack urls.

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # index view to client code that execute brute force
     path('<domain>/', views.index, name='index'),
+
+    # exfiltration of username/password
     path('<domain>/exfiltrate/', views.exfiltrate, name='exfiltrate'),
+
+    # get list of non ack usernames
     path('<domain>/usernames/', views.usernames, name='usernames'),
+
+    # POST the tested username offset
+    path('<domain>/usernames/ack', views.usernames, name='usernames'),
+
+    # get list of non ack passwords
     path('<domain>/passwords/', views.passwords, name='passwords'),
+
+    # POST the tested password offset
+    path('<domain>/passwords/ack', views.usernames, name='usernames'),
 ]
