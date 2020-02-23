9607cc2289e89c2939ebd655fdc49120a1e47e30
manufrommars <dev@lacourarie.org>
2020-02-23 23:16:57 +0100

    Add filldb command

---
+++ app/management/commands/filldb.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from app.models import Username, Password
+
+
+class Command(BaseCommand):
+    help = 'Fill the database with content of files in dict folder'
+
+    def handle(self, *args, **options):
+        try:
+            fake_probes()
+            self.stdout.write(self.style.SUCCESS('Successfully fill database'))
+        except Exception as e:
+            raise CommandError('Error : {}'.format(e))
+
+
+def fake_probes():
+    with open('dict/top-usernames-shortlist.txt') as file:
+        for line in file:
+            Username(username=line.rstrip()).save()
+    with open('dict/10-million-password-list-top-100.txt') as file:
+        for line in file:
+            Password(password=line.rstrip()).save()
