7e7b28d81042eac563283c2a7783c54fc17df70c
neuronaddict <dev@lacourarie.org>
2020-02-27 14:48:54 +0100

    Fix headless test for multi clients.

--- app/tests/test_headless_browser.py
+++ app/tests/test_headless_browser.py
@@ -1,18 +1,38 @@
 from threading import Thread
 
-from django.test import LiveServerTestCase
-from selenium.webdriver import Firefox
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import override_settings
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import Firefox, DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 
 from app.models import Domain, Client, Credential
 
 
-class HeadlessTest(LiveServerTestCase):
+class credentials_found(object):
+    """
+    An expectation for checking that credentials are found.
+    """
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __call__(self, driver):
+        return Credential.objects.filter(username=self.username, password=self.password).exists()
+
+
+@override_settings(DEBUG=True)
+class HeadlessTest(StaticLiveServerTestCase):
+    """
+    This test must be run with postgresql
+    """
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        domain = Domain(name='selenium', url='selenium.org', chunk_size=2)
+        domain = Domain(name='example', url='selenium.org', chunk_size=2)
         domain.save()
 
         for i in range(0, 10):
@@ -26,37 +46,51 @@ class HeadlessTest(LiveServerTestCase):
         options.add_argument('-headless')
         options.headless = True
 
+        d = DesiredCapabilities.FIREFOX
+        d['loggingPrefs'] = {'browser': 'ALL'}
+
         cls.drivers = [
-            Firefox(firefox_options=options),
-            Firefox(firefox_options=options),
-            Firefox(firefox_options=options),
-            Firefox(firefox_options=options)
+            Firefox(firefox_options=options, desired_capabilities=d),
+            Firefox(firefox_options=options, desired_capabilities=d),
+            Firefox(firefox_options=options, desired_capabilities=d),
+            Firefox(firefox_options=options, desired_capabilities=d)
         ]
 
     def test_exfiltration(self):
 
+        found_creds = False
         threads = []
 
         def visit_page(driver):
-            print('call {}'.format(self.live_server_url))
-            driver.get("{}/app/selenium/".format(self.live_server_url))
+            try:
+                global found_creds
+                driver.get("{}/app/example/".format(self.live_server_url))
+                wait = WebDriverWait(driver, 15)
+                try:
+                    wait.until(credentials_found('ansible', 'yankees'))
+                except TimeoutException as e:
+                    for entry in driver.get_log('browser'):
+                        print(entry)
+                    raise e
 
-        for i in range(0, 4):
-            t = Thread(target=visit_page, args=(self.drivers[i],))
+                print('creds found ...')
+                found_creds = True
+            finally:
+                driver.quit()
+
+        for driver in self.drivers:
+            t = Thread(target=visit_page, args=(driver,))
             t.start()
             threads.append(t)
 
         for t in threads:
             t.join()
 
-        self.assertTrue(Credential.objects.filter(username='ansible', password='yankees').exists(),
-                        'Credentials not found : {}'.format(list(Credential.objects.all())))
+        # self.assertTrue(found_creds, 'Credentials not found : {}'.format(list(Credential.objects.all())))
+        self.assert_credentials_found()
 
         print('End of test, clients : {}'.format(list(Client.objects.all())))
 
-    @classmethod
-    def tearDownClass(cls):
-        # noinspection PyUnresolvedReferences
-        for driver in cls.drivers:
-            driver.quit()
-        super().tearDownClass()
+    def assert_credentials_found(self):
+        self.assertTrue(Credential.objects.filter(username='ansible', password='yankees').exists(),
+                        'Credentials not found : {}'.format(list(Credential.objects.all())))
